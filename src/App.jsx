8c4e3c671919107ad8a548ab3a8c5f76f0f6c081
toby <leutoby@gmail.com>
2022-10-17 22:27:08 +0100

    more commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom';
 import { Flex } from '@chakra-ui/react';
 import NavBar from './components/Layout/NavBar';
-import Main from './components/Layout/Main';
 import Footer from './components/Layout/Footer';
-// import Home from './components/Parts/Home'
-// import Downloads from './components/Parts/Downloads'
-// import Installation from './components/Parts/Installation'
+import Home from './components/Pages/Home';
+import Downloads from './components/Pages/Downloads';
+import Installation from './components/Pages/Installation';
 // import Terms from './components/Parts/Terms'
-// import ContactUs from './components/Parts/ContactUs'
+import ContactUs from './components/Pages/ContactUs';
 import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
-      {/* <Route index element={<Home />} />
-      <Route path="downloads" element={<Downloads />} />
-      <Route path="how-to-install" element={<Installation />} />
-      <Route path="terms-of-service" element={<Terms />} />
-      <Route path="/contact-us" element={<ContactUs />} /> */}
-      {/* <Route path="*" element={<Home />} /> */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="downloads" element={<Downloads />} />
+        <Route path="how-to-install" element={<Installation />} />
+        {/* <Route path="terms-of-service" element={<Terms />} /> */}
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="*" element={<Home />} />
+      </Route>
     </Routes>
+
   );
 }
 
@@ -30,7 +31,6 @@ function Layout() {
     <Flex direction="column" flex="1">
       <NavBar />
       <Outlet />
-      <Main />
       <Footer />
     </Flex>
   );
--- src/components/Layout/Placeholder.jsx
+++
@@ -1,8 +0,0 @@
-import { Box } from '@chakra-ui/react';
-import * as React from 'react';
-
-function Placeholder() {
-  return (<Box role="presentation" py="3" px="4" color="on-accent" minH={80}>hello</Box>);
-}
-
-export default Placeholder;
