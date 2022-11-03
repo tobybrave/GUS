0f592b89813f3c34cb8852734af5b43de68d5e58
toby <leutoby@gmail.com>
2022-11-03 22:16:07 +0100

    fix center benefits

--- src/components/Parts/Benefits.jsx
+++ src/components/Parts/Benefits.jsx
@@ -1,63 +1,62 @@
 import React from 'react';
 import {
-  Flex,
   ListItem,
   ListIcon,
   List,
   Heading,
   Text,
+  Box,
+  Center,
 } from '@chakra-ui/react';
 import { MdCheckCircle } from 'react-icons/md';
 
 function Benefits() {
   return (
-    <Flex
-      m={5}
-      align="center"
-      justify="center"
-    >
+    <Box m={5}>
       <Heading fontSize="2xl" fontWeight={600} fontStyle="italic">
         Benefits
       </Heading>
-      <List spacing={5}>
-        <ListItem>
-          <ListIcon as={MdCheckCircle} boxSize={5} color="green.500" />
-          <Text as="span" fontSize="xl">
-            Grow and meet your target audience
-          </Text>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={MdCheckCircle} boxSize={5} color="green.500" />
-          <Text as="span" fontSize="xl">
-            Stay informed and entertained
-          </Text>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={MdCheckCircle} boxSize={5} color="green.500" />
-          <Text as="span" fontSize="xl">
-            Meet interesting people like you
-          </Text>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={MdCheckCircle} boxSize={5} color="green.500" />
-          <Text as="span" fontSize="xl">
-            Helps you discover new sources of income
-          </Text>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={MdCheckCircle} boxSize={5} color="green.500" />
-          <Text as="span" fontSize="xl">
-            Promote your business to real people
-          </Text>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={MdCheckCircle} boxSize={5} color="green.500" />
-          <Text as="span" fontSize="xl">
-            Monetize your content by charging your loyal viewers
-          </Text>
-        </ListItem>
-      </List>
-    </Flex>
+      <Center>
+        <List spacing={5}>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} boxSize={5} color="green.500" />
+            <Text as="span" fontSize="xl">
+              Grow and meet your target audience
+            </Text>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} boxSize={5} color="green.500" />
+            <Text as="span" fontSize="xl">
+              Stay informed and entertained
+            </Text>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} boxSize={5} color="green.500" />
+            <Text as="span" fontSize="xl">
+              Meet interesting people like you
+            </Text>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} boxSize={5} color="green.500" />
+            <Text as="span" fontSize="xl">
+              Helps you discover new sources of income
+            </Text>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} boxSize={5} color="green.500" />
+            <Text as="span" fontSize="xl">
+              Promote your business to real people
+            </Text>
+          </ListItem>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} boxSize={5} color="green.500" />
+            <Text as="span" fontSize="xl">
+              Monetize your content by charging your loyal viewers
+            </Text>
+          </ListItem>
+        </List>
+      </Center>
+    </Box>
   );
 }
 
