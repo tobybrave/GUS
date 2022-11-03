0afa3d238e389a50c8b4d4fa35a65d36c2173fb7
toby <leutoby@gmail.com>
2022-11-03 13:18:46 +0100

    rework sharing ui

--- src/components/Parts/Share.jsx
+++ src/components/Parts/Share.jsx
@@ -1,54 +1,82 @@
 import React from 'react';
 import {
-  Box, Button, Heading, Link, Text, Textarea,
+  Button,
+  Flex,
+  FormControl,
+  Heading,
+  Link,
+  Text,
+  Textarea,
+  Stack,
+  HStack,
+  useColorModeValue,
 } from '@chakra-ui/react';
 import { FaFacebook, FaTwitter, FaWhatsapp } from 'react-icons/fa';
 
-function Share() {
+export default function Share() {
   return (
-    <Box maxWidth="80%" m={5} mt={10}>
-      <Heading textAlign="left" as="h3" size="md" mt={3} mb={3}>
-        Would you like to share with your friends?
-      </Heading>
-      <Textarea
-        ml={5}
-        mr={5}
-        isReadOnly
-        placeholder={
-                    'Are You Tired of Getting Embarrassing WhatsApp Status Views?\n     🤷🏻‍♂️🤷🏻‍♂️\nVisit https://growursocials.com'
+    <Flex
+      // minH="50vh"
+      align="center"
+      justify="center"
+      bg={useColorModeValue('gray.50', 'gray.800')}
+    >
+      <Stack
+        spacing={4}
+        w="full"
+        maxW="md"
+        bg={useColorModeValue('white', 'gray.700')}
+        rounded="xl"
+        boxShadow="md"
+        p={6}
+        my={12}
+      >
+        <Heading lineHeight={1.1} fontSize={{ base: 'xl', md: '2xl' }}>
+          Would you like to share with your friends?
+        </Heading>
+        <FormControl id="share">
+          <Textarea
+            isReadOnly
+            _placeholder={{ color: 'gray.500' }}
+            placeholder={
+                  'Are You Tired of Getting Embarrassing WhatsApp Status Views?\n     🤷🏻‍♂️🤷🏻‍♂️\nVisit https://growursocials.com'
                 }
-      />
-      <Box>
-        <Text fontWeight="semibold" textAlign="center" mt={5}>
-          Or use the share buttons below
+          />
+        </FormControl>
+        <Text
+          fontWeight="semibold"
+          fontSize={{ base: 'sm', sm: 'md' }}
+          color={useColorModeValue('gray.800', 'gray.400')}
+        >
+          Or use the share button below
         </Text>
-        <Button colorScheme="whatsapp" m="2" variant="link">
-          <Link
-            href="https://wa.me/?text=Are%20You%20Tired%20of%20Getting%20Embarrassing%20WhatsApp%20Status%20Views%3F%20%F0%9F%98%B3%F0%9F%98%B3%F0%9F%A4%B7%F0%9F%8F%BB%E2%80%8D%E2%99%82%EF%B8%8F%F0%9F%A4%B7%F0%9F%8F%BB%E2%80%8D%E2%99%82%EF%B8%8F%F0%9F%91%89%20https%3A%2F%2Fgrowursocials.com"
-            isExternal
-          >
-            <FaWhatsapp size={30} />
-          </Link>
-        </Button>
-        <Button colorScheme="facebook" m="2" variant="link">
-          <Link
-            href="https://www.facebook.com/sharer/sharer.php?url=https://growursocials/?text=Are%20You%20Tired%20of%20Getting%20Embarrassing%20WhatsApp%20Status%20Views%3F%20%F0%9F%98%B3%F0%9F%98%B3%F0%9F%A4%B7%F0%9F%8F%BB%E2%80%8D%E2%99%82%EF%B8%8F%F0%9F%A4%B7%F0%9F%8F%BB%E2%80%8D%E2%99%82%EF%B8%8F%F0%9F%91%89%20https%3A%2F%2Fgrowursocials.com"
-            isExternal
-          >
-            <FaFacebook size={30} />
-          </Link>
-        </Button>
-        <Button colorScheme="twitter" m="2" variant="link">
-          <Link
-            href="https://twitter.com/intent/tweet?text=Are%20You%20Tired%20of%20Getting%20Embarrassing%20WhatsApp%20Status%20Views%3F%20%F0%9F%98%B3%F0%9F%98%B3%F0%9F%A4%B7%F0%9F%8F%BB%E2%80%8D%E2%99%82%EF%B8%8F%F0%9F%A4%B7%F0%9F%8F%BB%E2%80%8D%E2%99%82%EF%B8%8F%F0%9F%91%89%20https%3A%2F%2Fgrowursocials.com"
-            isExternal
-          >
-            <FaTwitter size={30} />
-          </Link>
-        </Button>
-      </Box>
-    </Box>
+        <HStack spacing={4} alignSelf="center">
+          <Button colorScheme="whatsapp" variant="link">
+            <Link
+              href="https://wa.me/?text=Are%20You%20Tired%20of%20Getting%20Embarrassing%20WhatsApp%20Status%20Views%3F%20%F0%9F%98%B3%F0%9F%98%B3%F0%9F%A4%B7%F0%9F%8F%BB%E2%80%8D%E2%99%82%EF%B8%8F%F0%9F%A4%B7%F0%9F%8F%BB%E2%80%8D%E2%99%82%EF%B8%8F%F0%9F%91%89%20https%3A%2F%2Fgrowursocials.com"
+              isExternal
+            >
+              <FaWhatsapp size={30} />
+            </Link>
+          </Button>
+          <Button colorScheme="facebook" variant="link">
+            <Link
+              href="https://www.facebook.com/sharer/sharer.php?url=https://growursocials/?text=Are%20You%20Tired%20of%20Getting%20Embarrassing%20WhatsApp%20Status%20Views%3F%20%F0%9F%98%B3%F0%9F%98%B3%F0%9F%A4%B7%F0%9F%8F%BB%E2%80%8D%E2%99%82%EF%B8%8F%F0%9F%A4%B7%F0%9F%8F%BB%E2%80%8D%E2%99%82%EF%B8%8F%F0%9F%91%89%20https%3A%2F%2Fgrowursocials.com"
+              isExternal
+            >
+              <FaFacebook size={30} />
+            </Link>
+          </Button>
+          <Button colorScheme="twitter" variant="link">
+            <Link
+              href="https://twitter.com/intent/tweet?text=Are%20You%20Tired%20of%20Getting%20Embarrassing%20WhatsApp%20Status%20Views%3F%20%F0%9F%98%B3%F0%9F%98%B3%F0%9F%A4%B7%F0%9F%8F%BB%E2%80%8D%E2%99%82%EF%B8%8F%F0%9F%A4%B7%F0%9F%8F%BB%E2%80%8D%E2%99%82%EF%B8%8F%F0%9F%91%89%20https%3A%2F%2Fgrowursocials.com"
+              isExternal
+            >
+              <FaTwitter size={30} />
+            </Link>
+          </Button>
+        </HStack>
+      </Stack>
+    </Flex>
   );
 }
-
-export default Share;
